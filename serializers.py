67b53b0344092379c0ebf4d7e099de227684d2d9
Mohamed Abdulhamed <41492751+Doomzy@users.noreply.github.com>
2021-08-01 18:24:36 +0200

    Modified the database and a serializer
    Modified the admin user information and the subject's serializer to make the academic year be (1st, 2nd,3rd)

---
+++ serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import Question, Subject
+
+class QuestionSerializer(serializers.ModelSerializer):
+
+    subject= serializers.ReadOnlyField(source='subject.name')
+    question_id= serializers.ReadOnlyField(source='id')
+
+    class Meta:
+        model= Question
+        fields= [
+            'subject', 'question_id',
+            'content',
+            'ans1','ans2',
+            'ans3','ans4',
+            'correct_answer'
+        ]
+
+class CreateQuestionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model= Question
+        fields= [
+            'subject',
+            'content',
+            'ans1','ans2',
+            'ans3','ans4',
+            'correct_answer'
+        ]
+
+class SubjectSerializer(serializers.ModelSerializer):
+
+    academic_year= serializers.ChoiceField(choices=Subject.ayChoices)
+
+    class Meta:
+        model= Subject
+        fields=['name', 'academic_year']
